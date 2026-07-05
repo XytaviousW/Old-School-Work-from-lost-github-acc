67d323842a5d351b16dcb01a3b7aaab800eba36a
XytaviousW <xytaviousw@gmail.com>
2026-07-05 07:49:42 -0500

    Add files via upload

---
+++ Ap-Price-Q1/115a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+//count << (insertion aperatior)
+// cin >> (extravtion operatior)
+// << endl: means the same as ln. (Enter)
+// std::cout << "Enter num1 "; (same as prompt)
+// std::cin >> num1; means read the previous prompt 
+int main()
+{
+ int i = 2;
+while (i <= 18) {
+  cout << i << endl;
+  i++;
+  i++;
+
+}
+}
+/*
+2
+4
+6
+8
+10
+12
+14
+16
+18
+*/
